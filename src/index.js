4ed8b761f279b182908acb3950a43425d51f94e0
Derrick Otte <derrickotte@gmail.com>
2022-04-29 19:27:37 -0400

    ADD chalk, triple check possible inputs
    - Added chalk for color output on commands
    - Made the commands have a lot more feedback
    - Triple checked all of the empty inputs (key/value etc.) that someone could put in

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 const { question } = require('readline-sync');
+const chalk = require('chalk');
+
+// Chalk classes
+const error = chalk.bold.red;
+const info = chalk.bold.cyan;
+const success = chalk.bold.green;
 
 function allKeysInDictionary(dictionary) {
   const dictionaryKeys = Object.keys(dictionary);
 
   if (dictionaryKeys.length) {
+    console.log(info('All keys in dictionary: '));
+
     dictionaryKeys.forEach((key, index) => {
       console.log(`${index + 1}) ${key}`);
     });
   } else {
-    console.log(`(empty set)`);
+    console.log(info('(empty set)'));
     console.log(`Dictionary: ${JSON.stringify(dictionary)}`);
   }
 }
@@ -17,21 +25,34 @@ function addMemberToKey(trimmedInput, dictionary) {
   const key = trimmedInput.shift();
   const value = trimmedInput.shift();
 
-  if (Object.keys(dictionary).includes(key)) {
-    if (dictionary[key] && dictionary[key].includes(value)) {
-      console.log(`ERROR, member ${value} already exists for ${key}`);
+  if (!key) {
+    console.log(error('ERROR: '));
+    console.log('You did not supply a key to add a member to.');
+    return;
+  } else {
+    if (!value) {
+      console.log(error('ERROR: '));
+      console.log(`You did not supply a member to add to the key of ${info(key)}`);
+      return;
+    }
+
+    if (Object.keys(dictionary).includes(key)) {
+      if (dictionary[key] && dictionary[key].includes(value)) {
+        console.log(error('ERROR:'))
+        console.log(`Member ${info(value)} already exists for ${info(key)}`);
+      } else {
+        dictionary[key].push(value);
+        addedMessage();
+      }
     } else {
+      dictionary[key] = [];
       dictionary[key].push(value);
       addedMessage();
     }
-  } else {
-    dictionary[key] = [];
-    dictionary[key].push(value);
-    addedMessage();
   }
 
   function addedMessage() {
-    console.log('Added');
+    console.log(success('Added'));
     console.log('Dictionary: ' + JSON.stringify(dictionary));
   }
 }
@@ -51,7 +72,7 @@ function allMembersInDictionary(dictionary) {
       console.log(`${index + 1}) ${member}`);
     });
   } else {
-    console.log(`(empty set)`);
+    console.log(info('(empty set)'));
     console.log(`Dictionary: ${JSON.stringify(dictionary)}`);
   }
 }
@@ -59,7 +80,12 @@ function allMembersInDictionary(dictionary) {
 function checkIfKeyExists(trimmedInput, dictionary) {
   const key = trimmedInput.shift();
 
-  console.log(Object.keys(dictionary).includes(key));
+  if (key) {
+    console.log(Object.keys(dictionary).includes(key));
+  } else {
+    console.log(error('ERROR: '));
+    console.log(`You did not supply a key to check if it exists.`);
+  }
 }
 
 function checkIfMemberExists(trimmedInput, dictionary) {
@@ -67,11 +93,24 @@ function checkIfMemberExists(trimmedInput, dictionary) {
   const member = trimmedInput.shift();
   const members = dictionary[key];
 
-  if (members) {
-    console.log(members.includes(member));
+  if (key) {
+    if (!member) {
+      console.log(error('ERROR: '));
+      console.log(`You did not supply the member to check if it exists in ${info(key)}.`);
+      return;
+    }
+
+    if (members) {
+      console.log(members.includes(member));
+    } else {
+      console.log(error('ERROR: '));
+      console.log(`Key of ${info(key)} does not exist in dictionary.`);
+    }
   } else {
-    console.log(`ERROR, key of ${key} does not exist in dictionary.`);
+    console.log(error('ERROR:'));
+    console.log('You did not supply a key for the member.');
   }
+
 }
 
 function clearDictionary(dictionary) {
@@ -79,7 +118,7 @@ function clearDictionary(dictionary) {
     delete dictionary[member];
   }
 
-  console.log(`Cleared dictionary.`)
+  console.log(success(`Cleared dictionary.`));
 }
 
 function itemsInDictionary(dictionary) {
@@ -97,7 +136,7 @@ function itemsInDictionary(dictionary) {
       })
     });
   } else {
-    console.log(`(empty set)`);
+    console.log(info('(empty set)'));
     console.log(`Dictionary: ${JSON.stringify(dictionary)}`);
   }
 }
@@ -106,14 +145,20 @@ function removeAllMembersFromKey(trimmedInput, dictionary) {
   const key = trimmedInput.shift();
   const members = dictionary[key];
 
-  if (members) {
-    // ADD sets a new empty array, so we can just delete here
-    delete dictionary[key];
-    console.log(`Removed`);
-    console.log('Dictionary: ' + JSON.stringify(dictionary));
+  if (key) {
+    if (members) {
+      // ADD sets a new empty array, so we can just delete here
+      delete dictionary[key];
+      console.log(success('Removed all members from ' + key));
+      console.log('Dictionary: ' + JSON.stringify(dictionary));
+    } else {
+      console.log(`ERROR, key of ${info(key)} does not exist in dictionary.`);
+    }
   } else {
-    console.log(`ERROR, key of ${key} does not exist in dictionary.`);
+    console.log(error('ERROR: '));
+    console.log('You did not supply a key to remove members from.');
   }
+
 }
 
 function removeMemberFromKey(trimmedInput, dictionary) {
@@ -121,36 +166,59 @@ function removeMemberFromKey(trimmedInput, dictionary) {
   const member = trimmedInput.shift();
   const members = dictionary[key];
 
-  if (members) {
-    const index = members.indexOf(member);
+  if (key) {
+    if (!member) {
+      console.log(error('ERROR: '));
+      console.log(`You did not supply the member to check if it exists in ${info(key)}.`);
+      return;
+    }
 
-    if (index > -1) {
-      members.splice(index, 1);
-
-      // You would never ever want to do this in a database
-      if (members.length === 0) {
-        delete dictionary[key];
+    if (members) {
+      const index = members.indexOf(member);
+  
+      if (index > -1) {
+        members.splice(index, 1);
+  
+        // You would never ever want to do this in a database
+        if (members.length === 0) {
+          delete dictionary[key];
+        }
+  
+        console.log(success(`Removed ${member}`));
+        console.log('Dictionary: ' + JSON.stringify(dictionary));
+      } else {
+        console.log(error('ERROR: '));
+        console.log(`${info(member)} does not exist in ${info(key)}`);
       }
-
-      console.log(`Removed ${member}`);
-      console.log('Dictionary: ' + JSON.stringify(dictionary));
+    } else {
+      console.log(error('ERROR: '));
+      console.log(`Key of ${info(key)} does not exist in dictionary.`);
     }
   } else {
-    console.log(`ERROR, key of ${key} does not exist in dictionary.`);
+    console.log(error('ERROR: '));
+    console.log('You did not supply a key to remove a member from.');
   }
+
 }
 
 function returnMembersOfKey(trimmedInput, dictionary) {
   const key = trimmedInput.shift();
   const members = dictionary[key];
 
-  if (members) {
-    members.forEach((member, index) => {
-      console.log(`${index + 1}) ${member}`);
-    });
+  if (key) {
+    if (members) {
+      members.forEach((member, index) => {
+        console.log(`${index + 1}) ${member}`);
+      });
+    } else {
+      console.log(error('ERROR: '));
+      console.log(`Key of ${info(key)} does not exist in dictionary.`);
+    }
   } else {
-    console.log(`ERROR, key of ${key} does not exist in dictionary.`);
+    console.log(error('ERROR: '));
+    console.log('You did not supply a key to get members from.');
   }
+
 }
 
 function detectCommand(input, dictionary) {
@@ -189,6 +257,7 @@ function detectCommand(input, dictionary) {
       removeAllMembersFromKey(trimmedInput, dictionary);
       break;
     default:
+      console.log(error('ERROR: '));
       console.log(`Command not recognized.`);
       break;
   }
